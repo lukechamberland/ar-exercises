ba043b8468da68074b782952e8bc46b985e4f452
Luke Chamberland <luke.chamberland@shaw.ca>
2023-06-30 09:02:43 +0000

    final lint

--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -8,4 +8,8 @@ require_relative './exercise_5'
 puts "Exercise 6"
 puts "----------"
 
-# Your code goes here ...
+@store1.employees.create(first_name: "Francis", last_name: "Donald", hourly_rate: 60)
+@store1.employees.create(first_name: "John", last_name: "Appleseed", hourly_rate: 50)
+@store1.employees.create(first_name: "Lucy", last_name: "Smith", hourly_rate: 55)
+@store2.employees.create(first_name: "Alice", last_name: "Jane", hourly_rate: 65)
+@store2.employees.create(first_name: "Chris", last_name: "Jackson", hourly_rate: 70)
