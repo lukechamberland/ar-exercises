0f1d0512272745b74b15121b5022fc5946f5386c
Luke Chamberland <luke.chamberland@shaw.ca>
2023-06-30 08:26:32 +0000

    Final lint

--- exercises/exercise_2.rb
+++ exercises/exercise_2.rb
@@ -4,4 +4,14 @@ require_relative './exercise_1'
 puts "Exercise 2"
 puts "----------"
 
-# Your code goes here ...
+class Store < ActiveRecord::Base
+end
+
+@store1 = Store.find(1)
+
+@store2 = Store.find(2)
+
+@store1.name = 'My new Store Name'
+@store1.save
+
+puts "First store updated. New name: #{@store1.name}"
