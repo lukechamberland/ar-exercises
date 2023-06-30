0b8b6a8cce2818591d55c5198d4ad04ef99d7f54
Luke Chamberland <luke.chamberland@shaw.ca>
2023-06-30 08:51:11 +0000

    final lint

--- exercises/exercise_5.rb
+++ exercises/exercise_5.rb
@@ -7,4 +7,14 @@ require_relative './exercise_4'
 puts "Exercise 5"
 puts "----------"
 
-# Your code goes here ...
+class Store < ActiveRecord::Base
+end
+
+total_revenue = Store.sum(:annual_revenue)
+puts "Total revenue is $#{total_revenue}  "
+
+average_revenue = Store.average(:annual_revenue)
+puts "Average Annual Revenue is $#{average_revenue}. "
+
+high_revenue_stores_count = Store.where('annual_revenue >= 1000000').count
+puts "Number of Stores with $1M+ Annual Sales is #{high_revenue_stores_count}. "
