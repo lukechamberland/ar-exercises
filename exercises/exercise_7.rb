d12ae76caf15afb056f40414553376cfcb7a9697
Luke Chamberland <luke.chamberland@shaw.ca>
2023-06-30 09:11:51 +0000

    final lint

--- exercises/exercise_7.rb
+++ exercises/exercise_7.rb
@@ -9,4 +9,18 @@ require_relative './exercise_6'
 puts "Exercise 7"
 puts "----------"
 
-# Your code goes here ...
+class Store < ActiveRecord::Base
+end
+
+puts "Enter store name: "
+
+store_name = gets.chomp
+
+new_store = Store.new(name: store_name)
+
+unless new_store.save
+  puts "Errors occurred while saving the store:"
+  new_store.errors.full_messages.each do |error|
+    puts error
+  end
+end
